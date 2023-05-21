2507e8e7810b667ddd17ea65b748c20562cec125
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-05-21 12:07:45 +0700

    Update fxmanifest.lua

--- DX_Talk_Marker/fxmanifest.lua
+++ DX_Talk_Marker/fxmanifest.lua
@@ -1,7 +1,10 @@
-
-description 'developed by DX'
 fx_version 'adamant' 
 game 'gta5'
+
+author '! D X H I G H#3690'
+description 'DX_Talk_Marker Script'
+version '0.1'
+
 client_scripts {
 	'config.lua',
 	'Code/client.lua',
